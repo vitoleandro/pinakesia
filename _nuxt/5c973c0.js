cfdf7134f7c5a151cb9fc1b2aed33105c4eff1b9
Leandro Vitor <vitoleandro@gmail.com>
2025-04-01 11:51:09 -0300

    primeiro commit

---
+++ _nuxt/5c973c0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{459:function(n,t,e){"use strict";e.r(t);var o={name:"Model3",props:{},data:function(){return{}}},l=e(77),component=Object(l.a)(o,(function(){return(0,this._self._c)("h1",[this._v("Model3")])}),[],!1,null,null,null);t.default=component.exports}}]);
